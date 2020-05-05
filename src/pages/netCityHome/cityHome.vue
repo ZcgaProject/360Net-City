331270f0112d93fb433deafca802302a00984aad
funcetse <m15210014607@163.com>
2020-05-05 14:08:58 +0800

    [add]备案号

--- src/pages/netCityHome/cityHome.vue
+++ src/pages/netCityHome/cityHome.vue
@@ -333,7 +333,7 @@
                 </div>
                 <div class="footerSheet_font">版权所有:中创国安信用管理股份有限公司</div>
                 <div class="footerSheet_font">Copyright©2001-2007 BFA All Rights Reserv</div>
-                <div class="footerSheet_font">ICP备案号：琼ICP备19004493号-1  </div>
+                <div class="footerSheet_font" @click="beianhao">ICP备案号：琼ICP备19004493号-1  </div>
                 <div class="footerSheet_kong"></div>
             </div>
             <div class="footerTabertwo"></div>
@@ -669,6 +669,9 @@
             nameDianji(event){
                 // console.log('出发event',event.target.value)
                 this.name = event.target.value
+            },
+            beianhao(){
+                location.href = 'http://www.beian.miit.gov.cn/'
             }
         }
 
